3cf51fca67c4ca2dbdb14ed4a6ae7e60fff0b144
Carlos Flores <cafg10@gmail.com>
2013-07-26 09:52:29 -0600

    Configurar Administrador de subdimension

--- src/Indira/SimoniBundle/Admin/SubDimensionAdmin.php
+++ src/Indira/SimoniBundle/Admin/SubDimensionAdmin.php
@@ -9,4 +9,27 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 
 class SubDimensionAdmin extends Admin
 {
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('nombre');
+        $formMapper->add('imagen');
+        $formMapper->add('color');
+        $formMapper->add('dimension');
+    }
+ 
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('nombre');
+        $datagridMapper->add('imagen');
+        $datagridMapper->add('color');
+        $datagridMapper->add('dimension');
+    }
+ 
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('nombre');
+        $listMapper->add('imagen');
+        $listMapper->add('color');
+        $listMapper->add('dimension');
+    }
 }
